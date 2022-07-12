3cbf1654a03f8066b591b1b0a1bd4611403e3f59
Yasser Emam <YasserEmam2002@gmail.com>
2022-07-12 22:24:03 +0200

    there are a lot of bugs

--- client/src/components/practice/Practice.js
+++ client/src/components/practice/Practice.js
@@ -1,96 +1,117 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../css/practice/Practice.css";
 
-class Practice extends React.Component {
-  getWords = async () => {
-    await fetch("http://localhost:5000/api/words")
-      .then((res) => {
-        return res.json();
-      })
+const answer = (ans, setWord, data, counter, setCounter) => {
+  // show which answer is right
+  // change the rank
+  // activate next button
+  // do th function of next button
+  // affect progress bar
+};
+
+const nextWord = (counter, setCounter, data, setWord) => {
+  // change word
+  console.log(data[counter].word);
+  setWord(data[counter].word);
+
+  // change the cards
+
+  // set counter
+  setCounter(counter + 1);
+};
+
+function Practice() {
+  let [word, setWord] = useState("...");
+  let [counter, setCounter] = useState(0);
+  let [data, setData] = useState([]);
+  let [rank, setRank] = useState(0);
+
+  let [nextBtn, setNextBtn] = useState(<button id="next">Next</button>);
+
+  // console.log(data);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/words")
+      .then((res) => res.json())
       .then((res) => {
         if (res.data) {
-          this.setState({ words: res.data });
-          // console.log(res.data);
+          // setWord(res.data[0].word);
+          setData(res.data);
+          setNextBtn(
+            <button
+              className="next disabled"
+              disabled="disabled"
+              onClick={() => {
+                nextWord(counter, setCounter, data, setWord);
+              }}
+            >
+              Next
+            </button>
+          );
+          // if first word add it to the page
+          if (counter === 0) {
+            nextWord(counter, setCounter, data, setWord);
+          }
         } else {
+          setData([]);
           throw new Error("Failed to get data from API");
         }
       })
       .catch((err) => {
         console.log(err);
       });
-  };
+  });
 
-  constructor(props) {
-    super(props);
+  return (
+    <div className="Practice">
+      <h1 className="word">{word}</h1>
 
-    this.state = {
-      words: [],
-      score: 0,
-      counter: 0,
-      word: "",
-    };
-
-    this.getWords();
-  }
-
-  componentDidMount() {
-    let word = this.state.words[0].word;
-    this.setState({ word: word });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.getWords);
-  }
-
-  answer(ans) {}
-
-  render() {
-    return (
-      <div className="Practice">
-        <h1 className="word">{this.state.word}</h1>
-
-        <div className="btn-row">
-          <button
-            id="btn-noun"
-            onClick={() => {
-              this.answer("noun");
-            }}
-          >
-            Noun
-          </button>
-          <button
-            id="btn-verb"
-            onClick={() => {
-              this.answer("verb");
-            }}
-          >
-            Verb
-          </button>
-        </div>
-
-        <div className="btn-row">
-          <button
-            id="btn-adj"
-            onClick={() => {
-              this.answer("adj");
-            }}
-          >
-            Adjective
-          </button>
-          <button
-            id="btn-adv"
-            onClick={() => {
-              this.answer("adv");
-            }}
-          >
-            Adverb
-          </button>
-        </div>
-
-        {/* Progress Bar */}
+      <div className="btn-row">
+        <button
+          id="btn-noun"
+          onClick={() => {
+            answer("noun", setWord);
+          }}
+        >
+          Noun
+        </button>
+        <button
+          id="btn-verb"
+          onClick={() => {
+            answer("verb", setWord);
+          }}
+        >
+          Verb
+        </button>
       </div>
-    );
-  }
+
+      <div className="btn-row">
+        <button
+          id="btn-adj"
+          onClick={() => {
+            answer("adj", setWord);
+          }}
+        >
+          Adjective
+        </button>
+        <button
+          id="btn-adv"
+          onClick={() => {
+            answer("adv", setWord);
+          }}
+        >
+          Adverb
+        </button>
+      </div>
+
+      <footer>
+        <p className="question-no">{counter} / 10</p>
+        {nextBtn}
+      </footer>
+
+      {/* Progress Bar */}
+    </div>
+  );
 }
 
 export default Practice;
--- server/build/server.js
+++ server/build/server.js
@@ -5,12 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 // import RateLimit from 'express-rate-limit';
 const index_1 = __importDefault(require("./routes/index"));
 const app = (0, express_1.default)();
 const port = 5000;
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use("/api", index_1.default);
 app.use((req, res) => {
     res.status(404).send("Error: route doesn't exist (-_-)");
